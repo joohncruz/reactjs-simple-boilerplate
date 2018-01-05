28c3737b41c697ab7c05225d433f43f2ff3fbc7a
joohncruz <joohncruzrocha@gmail.com>
2018-01-05 02:48:16 -0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import React from 'react'
+import Title from './title'
+
+const App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <Title name='Shunfps' />
+      </div>
+    )
+  }
+})
+
+export default App
