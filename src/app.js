d41d664d7e484ca180a8c6a2d6316ad4b951c5c8
joohncruz <joohncruzrocha@gmail.com>
2018-01-06 12:22:11 -0200

    eventos

--- src/app.js
+++ src/app.js
@@ -2,20 +2,18 @@
 
 import React, { Component } from 'react'
 import Square from './square'
-import Title from './title'
 
 class App extends Component {
   render () {
     return (
-      <div className='container'>
-        {['blue', 'red', 'green'].map((square, index) => (
-          <Square key={index} color={square} />
-        ))}
+      <div className='container' onClick={(e) => {
+        alert('clicou')
+      }}>
+        <Square />
       </div>
     )
   }
 }
-
 
 // const App = React.createClass({
 //   render: function () {
--- src/square.js
+++ src/square.js
@@ -3,15 +3,15 @@
 import React from 'react'
 
 const Square = ({ color }) => (
-	<div style={{
-		backgroundColor: color,
-		height: '100px',
-		width: '100px'
-	}} />
+  <div style={{
+    backgroundColor: color,
+    height: '100px',
+    width: '100px'
+  }} />
 )
 
 Square.defaultProps = {
-	color: 'red'
+  color: 'red'
 }
 
 export default Square
