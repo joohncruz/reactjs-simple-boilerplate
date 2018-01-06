302280e42d9928cd17f8af6f92874ba10f50c7f7
joohncruz <joohncruzrocha@gmail.com>
2018-01-06 10:40:49 -0200

    pure functions and render using map

--- src/app.js
+++ src/app.js
@@ -1,16 +1,30 @@
 'use strict'
 
-import React from 'react'
+import React, { Component } from 'react'
+import Square from './square'
 import Title from './title'
 
-const App = React.createClass({
-  render: function () {
+class App extends Component {
+  render () {
     return (
-      <div>
-        <Title name='Shunfps' />
+      <div className='container'>
+        {['blue', 'red', 'green'].map((square, index) => (
+          <Square key={index} color={square} />
+        ))}
       </div>
     )
   }
-})
+}
+
+
+// const App = React.createClass({
+//   render: function () {
+//     return (
+//       <div>
+//         <Title name='Shunfps' />
+//       </div>
+//     )
+//   }
+// })
 
 export default App
--- src/title.js
+++ src/title.js
@@ -2,12 +2,27 @@
 
 import React from 'react'
 
-const Title = React.createClass({
-  render: function () {
-    return (
-      <h1>Olá {this.props.name}</h1>
-    )
-  }
-})
+const Title = ({ name, lastname }) => (
+  <h1> Olá Shun {`${name} ${lastname}!`} </h1>
+)
+
+Title.defaultProps = {
+  name: 'Desconhecido',
+  lastname: 'Sem sobrenome'
+}
+
+// const Title = React.createClass({
+//   getDefaultProps: function () {
+//     return {
+//       name: 'Desconhecido',
+//       lastname: 'Sem sobrenome'
+//     }
+//   },
+//   render: function () {
+//     return (
+//       <h1>Olá {this.props.name}</h1>
+//     )
+//   }
+// })
 
 export default Title
