8182e5179f85d3faea2483087e59f71d15163464
Jonathan Cruz <joohncruzrocha@gmail.com>
2018-04-05 23:55:16 -0300

    Um pouco sobre lifecycle e propTypes

--- learning-state/src/app.js
+++ learning-state/src/app.js
@@ -2,29 +2,14 @@
 
 import React, { Component } from 'react'
 import Button from './button'
-import Square from './square'
-import Title from './title'
 
 class App extends Component {
-  constructor () {
-    super()
-    this.state = {
-      color: 'green'
-    }
-  }
   render () {
     return (
       <div>
-        <Title name='Archer' />
-        <Square color={this.state.color} />
-
-        {['red', 'green', 'blue'].map((color) => {
-          <Button 
-            key={color}
-            handleClick={() => this.setState({ color })}>
-            {color}
-          </Button>
-        })}
+        <Button handleClick={() => console.log('clicou')}>
+          Clique em mim
+        </Button>
       </div>
     )
   }
